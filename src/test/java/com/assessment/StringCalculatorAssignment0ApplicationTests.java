83615af22f786011f45e4410575861bff277e584
pavanmoguluri <moguluripavankumar@gmail.com>
2024-12-27 16:19:32 +0530

    added documented test cases

--- src/test/java/com/assessment/StringCalculatorAssignment0ApplicationTests.java
+++ src/test/java/com/assessment/StringCalculatorAssignment0ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.assessment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,52 @@ class StringCalculatorAssignment0ApplicationTests {
 		});
 	}
 	
+	@Test
+	public void onlyChars() {
+		String inpStr = ",y,x";
+		assertEquals(0,stringCalculator.Add(inpStr));
+	}
 	
+	@Test
+	public void noNumOrChar() {
+		String inpStr = ",,";
+		assertEquals(0,stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void gt1000() {
+		String inpStr = "1,1002";
+		assertEquals(1,stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void gt1000False() {
+		String inpStr = "1,1002";
+		assertFalse(1003 == stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void semicolonDelimeter() {
+		String inpStr = "//;\\n1;2";
+		assertEquals(3,stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void delimOfAnyLen() {
+		String inpStr = "//[***]\\n1***2***3";
+		assertEquals(6,stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void multipleDelims() {
+		String inpStr = "//[*][%]\n1*2%3";
+		assertEquals(6,stringCalculator.Add(inpStr));
+	}
+	
+	@Test
+	public void multipleDelimsLongLength() {
+		String inpStr = "//[**][%%]\\n1**2%%3";
+		assertEquals(6,stringCalculator.Add(inpStr));
+	}
 
 }
