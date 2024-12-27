00929f2fa91adac5100468e532e48074ba1ab815
pavanmoguluri <moguluripavankumar@gmail.com>
2024-12-27 16:09:19 +0530

    method to extract numbers from string

--- src/main/java/com/assessment/StringCalculator.java
+++ src/main/java/com/assessment/StringCalculator.java
@@ -3,35 +3,59 @@ package com.assessment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
-	
+
+	// method to extract numbers in the string(+ve & -ve)
+	private List<Integer> extractNumbers(String str) {
+		str += " ";
+		List<String> arr = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		for (char c : str.toCharArray()) {
+			if (Character.isDigit(c) || (sb.length() == 0 && c == '-')) {
+				sb.append(c);
+			} else {
+				if (sb.length() > 0) {
+					arr.add(sb.toString());
+					sb = new StringBuilder();
+				}
+				if (c == '-') {
+					sb.append(c);
+				}
+			}
+		}
+		return arr.stream().map(Integer::parseInt).collect(Collectors.toList());
+	}
+
 	public int Add(String numbers) {
-        int sum = 0;
-        List<Integer> pVeList = new ArrayList<Integer>();
-        List<Integer> nVeList = new ArrayList<Integer>();
-            if(null == numbers || numbers.isBlank()) return 0;
-            String[] numberArr = numbers.split(",");
-            for(String str : numberArr){
-                try{
-                    int num = Integer.parseInt(str);
-                    if(num<0) nVeList.add(num);
-                    else pVeList.add(num);
-                }catch (Exception e){
-                    System.out.println("The character is not a integer- "+str);
-                }
-            }
-            if(!nVeList.isEmpty()) {
-                StringJoiner nVeStr = new StringJoiner(",");
-                for(Integer nVeValue : nVeList){
-                    nVeStr.add(String.valueOf(nVeValue));
-                }
-                throw new IllegalArgumentException("negatives not allowed- "+ nVeStr);
-            }
-            for(Integer pVeNum : pVeList){
-                sum += pVeNum;
-            }
-        return sum;
-    }
+
+		if (null == numbers || numbers.isBlank())
+			return 0;
+		int sum = 0;
+		List<Integer> pVeList = new ArrayList<>();
+		List<Integer> nVeList = new ArrayList<>();
+		List<Integer> nums = extractNumbers(numbers);
+		for (Integer num : nums) {
+			if (num < 0)
+				nVeList.add(num);
+			else {
+				if (num > 1000)
+					num = 0;
+				pVeList.add(num);
+			}
+		}
+		if (!nVeList.isEmpty()) {
+			StringJoiner nVeStr = new StringJoiner(",");
+			for (Integer nVeValue : nVeList) {
+				nVeStr.add(String.valueOf(nVeValue));
+			}
+			throw new IllegalArgumentException("negatives not allowed- " + nVeStr);
+		}
+		for (Integer pVeNum : pVeList) {
+			sum += pVeNum;
+		}
+		return sum;
+	}
 
 }
